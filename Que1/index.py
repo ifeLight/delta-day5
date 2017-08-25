d73ba8ed33d16d8f652c3e3fff7cfd15e2f4f2a6
Karim Ifedayo <karimjnr@gmail.com>
2017-08-25 15:03:47 -0700

    Que 1 solved

---
+++ Que1/index.py
@@ -0,0 +1,11 @@
+import re
+
+
+fileinput = open("tester.txt")
+text = fileinput.read();
+
+sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', text)
+
+fileinput.close();
+for stuff in sentences:
+        print(stuff) 
---
+++ Que1/readme.md
@@ -0,0 +1,3 @@
+# Question1 answer day 4
+
+Run the index.py file, You can change file name in line 7  for testing
